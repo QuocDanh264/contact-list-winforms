df12301b6c6344f9bfa98acd6301d2f3daa3d529
Quoc Danh <danh99171@gmail.com>
2025-03-27 22:17:10 +0700

    Khởi tạo dự án Windows Forms với MainForm

---
+++ MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ContactList
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnthemlienhe = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnthemlienhe
+            // 
+            this.btnthemlienhe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.857143F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthemlienhe.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnthemlienhe.Location = new System.Drawing.Point(280, 208);
+            this.btnthemlienhe.Name = "btnthemlienhe";
+            this.btnthemlienhe.Size = new System.Drawing.Size(197, 65);
+            this.btnthemlienhe.TabIndex = 0;
+            this.btnthemlienhe.Text = "Thêm liên hệ";
+            this.btnthemlienhe.UseVisualStyleBackColor = true;
+            this.btnthemlienhe.Click += new System.EventHandler(this.btnthemlienhe_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.14286F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(252, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh sách liên hệ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnthemlienhe);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnthemlienhe;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
