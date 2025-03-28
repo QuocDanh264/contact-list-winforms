bb30172a43f78d27d46e9c93d0a0d6dddd0e01fb
Mydyn71 <mydyn71@gmail.com>
2025-03-28 13:10:21 +0700

    Thêm Form để thêm liên hệ mới

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -36,9 +36,10 @@
             // 
             this.btnthemlienhe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.857143F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnthemlienhe.ForeColor = System.Drawing.Color.IndianRed;
-            this.btnthemlienhe.Location = new System.Drawing.Point(280, 208);
+            this.btnthemlienhe.Location = new System.Drawing.Point(233, 174);
+            this.btnthemlienhe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnthemlienhe.Name = "btnthemlienhe";
-            this.btnthemlienhe.Size = new System.Drawing.Size(197, 65);
+            this.btnthemlienhe.Size = new System.Drawing.Size(161, 54);
             this.btnthemlienhe.TabIndex = 0;
             this.btnthemlienhe.Text = "Thêm liên hệ";
             this.btnthemlienhe.UseVisualStyleBackColor = true;
@@ -49,19 +50,21 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.14286F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(252, 89);
+            this.label1.Location = new System.Drawing.Point(206, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(247, 31);
+            this.label1.Size = new System.Drawing.Size(220, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Danh sách liên hệ";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(655, 375);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnthemlienhe);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
