af0a34974fa45455d13c20cfa38d6b2a02e76ab4
Mydyn71 <mydyn71@gmail.com>
2025-03-28 21:48:10 +0700

    Xóa Form thêm liên hệ

--- MainForm.cs
+++ MainForm.cs
@@ -19,8 +19,7 @@ namespace ContactList
 
         private void btnthemlienhe_Click(object sender, EventArgs e)
         {
-            AddContactForm addForm = new AddContactForm();
-            addForm.ShowDialog();
+           
 
         }
     }
